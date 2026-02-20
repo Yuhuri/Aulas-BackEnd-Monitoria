ab4b5bc4dd1ab74a48896d0041894c672127881d
Aula Demo 6 <aulademo6@techers.local>
2026-02-20 17:42:49 -0300

    exercicio da calculadora de area

--- aula3/app.js
+++ aula3/app.js
@@ -1,1 +1,11 @@
-console.log("OOOOOI")
+// Calculadora de área de terreno (largura * comprimento)
+
+function Area(l, c){
+    return(l*c)
+}
+
+console.log(Area(3, 4))
+
+function AreaTri(l, c){
+    return(l*c/2)
+}
